d546ab9f647f23d7c62252e4eb793504a9ad07d3
ScDor <18174994+ScDor@users.noreply.github.com>
2024-06-29 13:36:40 +0300

    everything else

---
+++ README.md
@@ -0,0 +1,29 @@
+# Prevent Poetry Downgrade
+
+This [pre-commit](https://pre-commit.com) hook ensures that the *version of Poetry* used to manage a project's dependencies does not get inadvertently downgraded when editing the `poetry.lock` file.
+It prevents committing changes that would lower the Poetry version, encouraging best practices in dependency management.
+
+## Usage
+
+Copy this to your [`.pre-commit-config`](https://pre-commit.com/#plugins) file
+
+```yaml
+- repo: https://github.com/ScDor/prevent-poetry-downgrade
+  hooks:
+  - id: prevent-poetry-downgrade
+    rev: v0.1.0
+```
+
+To pass other arguments, add the `args` key, e.g.
+```yaml
+    args: ["--strict"]
+```
+
+**Arguments**:
+
+* `lock-path`: Defaults to a `poetry.lock` in the current directory.
+
+**Options**:
+
+* `--prev-ref`: Git reference to the previous version of the lock file.  [default: HEAD]
+* `--strict`: Whether to require that the exact same version of Poetry is used. Otherwise, will promote upgrading Poetry to the latest version.
---
+++ prevent_poetry_downgrade.py
@@ -0,0 +1,90 @@
+"""Prevent downgrading Poetry when editing a lock file."""
+
+from pathlib import Path
+import re
+from packaging.version import Version
+import git
+import typer
+
+VERSION_RE = re.compile(r"Poetry (\d+\.\d+\.\d+)")
+POETRY_LOCK_FILE_NAME = "poetry.lock"
+
+
+def parse_version(lock_header: str) -> Version:
+    """Parse the Poetry version from the lock file header."""
+    if not lock_header.startswith("# This file is automatically"):
+        raise ValueError(f"Invalid lock file header: {lock_header}")
+
+    if not (match := VERSION_RE.search(lock_header)):
+        raise ValueError(f"Cannot parse version from {lock_header}")
+
+    return Version(match.group(1))
+
+
+def _find_current_version(lock_path: Path) -> Version:
+    with lock_path.open() as f:
+        current_header = f.readline()
+    return parse_version(current_header)
+
+
+def _find_previous_version(lock_path: Path, head: str = "HEAD") -> Version:
+    try:
+        previous_lock_content = git.Repo(
+            lock_path, search_parent_directories=True
+        ).git.cat_file("blob", f"{head}:{POETRY_LOCK_FILE_NAME}")
+    except Exception as e:
+        typer.echo(f"Cannot find previous lock file {type(e)} {e}")
+        raise typer.Exit(1) from e
+
+    try:
+        previous_lock_header = previous_lock_content.split("\n")[0]
+    except Exception as e:
+        typer.echo(f"Cannot parse previous lock file: {type(e)} {e}")
+        raise typer.Exit(1) from e
+
+    return parse_version(previous_lock_header)
+
+
+def main(
+    lock_path: Path = typer.Argument(
+        default=Path.cwd() / POETRY_LOCK_FILE_NAME,
+        help=f"Path to the lock file. Defaults to {POETRY_LOCK_FILE_NAME} in the current directory.",
+        dir_okay=False,
+        file_okay=True,
+        exists=True,
+    ),
+    prev_ref: str = typer.Option(
+        "HEAD", help="Git reference to the previous version of the lock file."
+    ),
+    require_same_version: bool = typer.Option(
+        False,
+        "--strict",
+        help="Whether to require that the exact same version of Poetry is used. "
+        "Otherwise, will promote upgrading Poetry to the latest version.",
+    ),
+) -> None:
+    """Prevent downgrading Poetry when editing a lock file."""
+    current_version = _find_current_version(lock_path)
+    previous_version = _find_previous_version(lock_path, prev_ref)
+
+    if require_same_version:
+        if previous_version != current_version:
+            typer.echo(
+                "Poetry.lock version mismatch detected.\n"
+                f"You are using Poetry {current_version} to edit poetry.lock previously generated with Poetry {previous_version}.\n"
+                f"Run `poetry self update {previous_version}` to match your Poetry version."
+            )
+            raise typer.Exit(1)
+
+    else:
+        if previous_version > current_version:
+            typer.echo(
+                "Poetry.lock downgrade detected.\n"
+                f"You are using Poetry {current_version} to edit poetry.lock previously generated with Poetry {previous_version}.\n"
+                "Run `poetry self update` to update your Poetry version."
+            )
+            raise typer.Exit(1)
+
+
+if __name__ == "__main__":
+    typer.run(main)
